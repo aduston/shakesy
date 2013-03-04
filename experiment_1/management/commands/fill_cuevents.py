1811f5244cbc7eea3c4f837937630519bd472a13
Aaron Hill <aa1ronham@aol.com>
2013-03-03 20:05:50 -0500

    Use different parser for SRT

--- experiment_1/management/commands/fill_cuevents.py
+++ experiment_1/management/commands/fill_cuevents.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from pysrt import SubRipFile
 from experiment_1 import models
+import re
 import os
 
 CHARACTERS = {'1': models.Character(name="Barnardo", title="Barnardo", description="Barnardo"),
@@ -20,26 +20,28 @@ class Command(BaseCommand):
         contemporary_subs = self._parse("contemporary_text.srt")
 
         i = 0
-        for sub in original_subs:
-            start = sub.start.hours * 36000 + sub.start.minutes * 600 + sub.start.seconds * 10 + sub.start.milliseconds / 100.0
-            print "Subtitle %d" % sub.index
-            models.Subtitle(contemporary_text=contemporary[i].text[2:], original_text=sub.text[2:], character=CHARACTERS[sub.text[0]], start_time=start).save()
+        for orig_sub, contemp_sub in zip(original_subs, contemporary_subs):
+            print orig_sub
+            start = int(orig_sub[0]) * 36000 + int(orig_sub[1]) * 600 + int(orig_sub[2]) * 10 + int(orig_sub[3]) / 100.0
+            print "Subtitle %d" % i
+            models.Subtitle(contemporary_text=contemp_sub[9], original_text=orig_sub[9], character=CHARACTERS[orig_sub[8]], start_time=start).save()
             i += 1
 
     def _parse(self, file_name):
-        file_name = os.path.join(settings.DATA_DIR, file_name)        
+        file_name = os.path.join(settings.DATA_DIR, file_name)
         with open(file_name, "r") as f:
             return self._parse_text(f.read())
 
     def _parse_text(self, text):
         pattern = re.compile(self._pattern(), re.DOTALL)
-        match = pattern.search(text)
-        
+        match = pattern.findall(text)
+        return match
+
 
     def _pattern(self):
-        pattern = r'\d+\s*?\n'
-        pattern += r'(?P<s_hour>\d{2}):(?P<s_min>\d{2}):(?P<s_sec>\d{2})(,(?P<s_secfr>\d*))?'
+        pattern = r'\d+\s*?\r?\n'
+        pattern += r'(?P<s_hour>\d{2}):(?P<s_min>\d{2}):(?P<s_sec>\d{2}),(?P<s_secfr>\d*)'
         pattern += r' --> '
-        pattern += r'(?P<e_hour>\d{2}):(?P<e_min>\d{2}):(?P<e_sec>\d{2})(,(?P<e_secfr>\d*))?'
-        pattern += r'\n(\n|(?P<text>.+?)\n\n)'
+        pattern += r'(?P<e_hour>\d{2}):(?P<e_min>\d{2}):(?P<e_sec>\d{2}),(?P<e_secfr>\d*)\r?'
+        pattern += r'\n(\d+):(?P<text>.+?)\r?\n'
         return pattern
