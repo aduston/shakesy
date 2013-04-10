54531f628bb06b945bf9b31e701576f82c7d8db9
Aaron Hill <aa1ronham@aol.com>
2013-04-09 20:54:36 -0400

    Created experiment_1c

--- experiment_1/views.py
+++ experiment_1/views.py
@@ -48,3 +48,29 @@ def index_b(request):
           "characters": json.dumps(char_dicts),
           "subtitles": json.dumps(subs_dicts) },
         context_instance=RequestContext(request))
+
+def index_c(request):
+    yt_query_string = urllib.urlencode(
+        { "enablejsapi": "1",
+          "origin": settings.ORIGIN })
+    subs_dicts = [
+            { "start_time": cs.start_time,
+              "character_id": cs.character_id,
+              "contemporary_text": cs.contemporary_text,
+              "original_text": cs.original_text }
+            for cs in models.Subtitle.objects.all().order_by("start_time")]
+
+    char_dicts = dict([
+            [c.id,
+             { "id": c.id,
+               "name": c.name,
+               "title": c.title, 
+               "image": c.cimage.url if c.cimage else "" }]
+            for c in models.Character.objects.all()])
+
+    return render_to_response(
+        "experiment_1c.html",
+        { "yt_query_string": yt_query_string,
+          "characters": json.dumps(char_dicts),
+          "subtitles": json.dumps(subs_dicts) },
+        context_instance=RequestContext(request))
